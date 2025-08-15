06a213d17e7b33eec5bf87ffbad9c2679f676b8d
Angad Sudan <angadsudan453@gmail.com>
2025-08-15 14:48:36 +0530

    added some try catch block

--- src/app/(api)/generate/route.ts
+++ src/app/(api)/generate/route.ts
@@ -10,32 +10,56 @@ const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+const fallbackMessages = {
+  HITESH_SIR:
+    "Ruko zara, pehele chai pe aaein. Phir aapke sawalon ka jawab dete hain",
+  PIYUSH_SIR: "Bas 2 minute, aata hoon!",
+};
+
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  const { userMessage, messages, currentMentor } = body;
+  const {
+    userMessage,
+    messages,
+    currentMentor,
+  }: {
+    userMessage: string;
+    messages: any[];
+    currentMentor: "HITESH_SIR" | "PIYUSH_SIR";
+  } = body;
+  try {
+    let currentPrompt =
+      currentMentor === "HITESH_SIR" ? HITESH_SIR_PROMPT : PIYUSH_SIR_PROMPT;
 
-  let currentPrompt =
-    currentMentor === "HITESH_SIR" ? HITESH_SIR_PROMPT : PIYUSH_SIR_PROMPT;
+    currentPrompt = currentPrompt.replace("{user_message}", userMessage);
+    currentPrompt = currentPrompt.replace(
+      "{prev_chat}",
+      JSON.stringify(messages)
+    );
 
-  currentPrompt = currentPrompt.replace("{user_message}", userMessage);
-  currentPrompt = currentPrompt.replace(
-    "{prev_chat}",
-    JSON.stringify(messages)
-  );
+    const response = await client.chat.completions.create({
+      model: "gpt-4.1-mini",
+      messages: [
+        { role: "system", content: currentPrompt },
+        { role: "user", content: userMessage },
+      ],
+    });
 
-  const response = await client.chat.completions.create({
-    model: "gpt-4.1-mini",
-    messages: [
-      { role: "system", content: currentPrompt },
-      { role: "user", content: userMessage },
-    ],
-  });
-
-  return new Response(
-    JSON.stringify({
-      success: true,
-      message: response.choices[0].message.content,
-    }),
-    { status: 201, headers: { "Content-Type": "application/json" } }
-  );
+    return new Response(
+      JSON.stringify({
+        success: true,
+        message: response.choices[0].message.content,
+      }),
+      { status: 201, headers: { "Content-Type": "application/json" } }
+    );
+  } catch (error) {
+    console.log(error);
+    return new Response(
+      JSON.stringify({
+        success: true,
+        message: fallbackMessages[currentMentor],
+      }),
+      { status: 201, headers: { "Content-Type": "application/json" } }
+    );
+  }
 }
